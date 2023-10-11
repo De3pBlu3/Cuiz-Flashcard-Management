73386d62edbc3d3a614af8f55d17f5ad29d00399
23357614 <jackcasey614@gmail.com>
2023-10-11 14:11:23 +0100

    Creating tables using SQL!

--- DBTest.java
+++ DBTest.java
@@ -1,7 +1,8 @@
+
+
 public class DBTest {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-//        DB_SetUp.createNewDatabase(".db");
+        DB_SetUp.createNewDatabase(".db");
         DB_CreateTables.CreateStandardTables();
     }
 }
--- DB_Handler/src/DB_CreateTables.java
+++ DB_Handler/src/DB_CreateTables.java
@@ -1,4 +1,3 @@
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -13,19 +12,45 @@ public class DB_CreateTables {
         String url = "jdbc:sqlite:" + DquizPath +"/" + "DQuiz.db";
 
         // SQL statement for creating a new table
-        String sql =
+        String sql_create_table_cards =
                 """
-                CREATE TABLE IF NOT EXISTS cards (
-                	card_id integer PRIMARY KEY,
-                	name text NOT NULL,
-                	capacity real
-                );
-                """;
+            CREATE TABLE IF NOT EXISTS cards (
+                "card_id"	TEXT NOT NULL,
+                "question"	TEXT,
+                "answers"	TEXT,
+                "correct_answer_index"	INTEGER,
+                "category"	INTEGER,
+                "difficulty"	INTEGER,
+                PRIMARY KEY("card_id")
+            ) WITHOUT ROWID;
+                    """;
+        String sql_create_table_user =
+                """
+                    CREATE TABLE IF NOT EXISTS users (
+                        "user_ID"	TEXT,
+                        "password"	TEXT,
+                        PRIMARY KEY("user_ID")
+                    ); WITHOUT ROWID;
+                                """;
+
+        String sql_create_table_score =
+                """
+                CREATE TABLE "scores" (
+                    "score"	INTEGER,
+                    "card_id"	TEXT,
+                    "user_ID"	TEXT,
+                    FOREIGN KEY("card_id") REFERENCES "cards"("card_id"),
+                    FOREIGN KEY("user_ID") REFERENCES "users"("user_ID")
+                );""";
+
 
         try (Connection conn = DriverManager.getConnection(url);
              Statement stmt = conn.createStatement()) {
             // create a new table
-            stmt.execute(sql);
+            stmt.execute(sql_create_table_cards); 
+            stmt.execute(sql_create_table_user);
+            stmt.execute(sql_create_table_score);
+            System.out.println("Tables created");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
