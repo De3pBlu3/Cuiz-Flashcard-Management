3a10cdcaee54e11b645a809e7626c5b85270e226
23357614 <jackcasey614@gmail.com>
2023-10-16 22:29:18 +0100

    Created class for adding a win, adding a loss and altering score for each card/user association. Automatically creates assoication if first time being played

--- DB_Handler/src/DB_ScoreInteract.java
+++ DB_Handler/src/DB_ScoreInteract.java
@@ -6,7 +6,7 @@ public class DB_ScoreInteract {
         String sql = "SELECT score, wins, losses FROM scores WHERE user_ID = ? AND card_id = ?";
 
         try (Connection conn = DB_ConnCreator.connect();
-             java.sql.PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            java.sql.PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
             // set the value
             pstmt.setString(2, card_id);
@@ -20,13 +20,13 @@ public class DB_ScoreInteract {
                 int score = rs.getInt("score");
                 wins++;
                 score++;
-                changeWins(user_id, card_id, wins);
-                changeScore(user_id, card_id, score);
+                changeWins(user_id, card_id, wins, conn);
+                changeScore(user_id, card_id, score, conn);
             } else {
                 // if record doesn't exist, create it, add 1 to wins, and change score by 1
-                createRecord(user_id, card_id);
-                changeWins(user_id, card_id, 1);
-                changeScore(user_id, card_id, 1);
+                createRecord(user_id, card_id, conn);
+                changeWins(user_id, card_id, 1, conn);
+                changeScore(user_id, card_id, 1, conn);
             }
 
         } catch (SQLException e) {
@@ -34,11 +34,42 @@ public class DB_ScoreInteract {
         }
     }
 
-    public static void createRecord(String user_id, String card_id) {
-        String sql = "INSERT INTO scores(user_ID, card_id) VALUES(?,?)";
+    public static void addLoss(String user_id, String card_id) {
+        String sql = "SELECT score, wins, losses FROM scores WHERE user_ID = ? AND card_id = ?";
 
         try (Connection conn = DB_ConnCreator.connect();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            java.sql.PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            // set the value
+            pstmt.setString(2, card_id);
+            pstmt.setString(1, user_id);
+            //
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                // if record exists, add 1 to losses, and change score by -1
+                int losses = rs.getInt("losses");
+                int score = rs.getInt("score");
+                losses++;
+                score--;
+                changeLosses(user_id, card_id, losses, conn);
+                changeScore(user_id, card_id, score, conn);
+            } else {
+                // if record doesn't exist, create it, add 1 to losses, and change score by -1
+                createRecord(user_id, card_id, conn);
+                changeLosses(user_id, card_id, 1, conn);
+                changeScore(user_id, card_id, -1, conn);
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void createRecord(String user_id, String card_id, Connection conn) {
+        String sql = "INSERT INTO scores(user_ID, card_id) VALUES(?,?)";
+
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, user_id);
             pstmt.setString(2, card_id);
             pstmt.executeUpdate();
@@ -46,11 +77,10 @@ public class DB_ScoreInteract {
             System.out.println(e.getMessage());
         }
     }
-    public static void changeWins(String user_id, String card_id, int wins) {
+    public static void changeWins(String user_id, String card_id, int wins, Connection conn) {
         String sql = "UPDATE scores SET wins = ? WHERE user_ID = ? AND card_id = ?";
 
-        try (Connection conn = DB_ConnCreator.connect();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setInt(1, wins);
             pstmt.setString(2, user_id);
             pstmt.setString(3, card_id);
@@ -59,11 +89,10 @@ public class DB_ScoreInteract {
             System.out.println(e.getMessage());
         }
     }
-    public static void changeScore(String user_id, String card_id, int score) {
+    public static void changeScore(String user_id, String card_id, int score, Connection conn) {
         String sql = "UPDATE scores SET score = ? WHERE user_ID = ? AND card_id = ?";
 
-        try (Connection conn = DB_ConnCreator.connect();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setInt(1, score);
             pstmt.setString(2, user_id);
             pstmt.setString(3, card_id);
@@ -73,11 +102,10 @@ public class DB_ScoreInteract {
         }
     }
 
-    public static void changeLosses(String user_id, String card_id, int losses) {
+    public static void changeLosses(String user_id, String card_id, int losses, Connection conn) {
         String sql = "UPDATE scores SET losses = ? WHERE user_ID = ? AND card_id = ?";
 
-        try (Connection conn = DB_ConnCreator.connect();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setInt(1, losses);
             pstmt.setString(2, user_id);
             pstmt.setString(3, card_id);
@@ -87,7 +115,4 @@ public class DB_ScoreInteract {
         }
     }
 
-    public static void main(String[] args) {
-        addWin("AAAA", "john");
-    }
 }
