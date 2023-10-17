8bbf29d4038579693bbb988fd90a4ab97018508a
23357614 <jackcasey614@gmail.com>
2023-10-17 02:51:34 +0100

    Implimenting increasing difficulty play

--- playLoop.java
+++ playLoop.java
@@ -4,14 +4,12 @@ public class playLoop {
 
 
         // we are going to aasume user has been logged in, we might pass this as a parameter when the user logs in
-        // so for now, we will create user_id var and assign it a temp value
-        String user_id = "Sean";
-
-        // we will also assume the player has chosen a game mode, and will run a different method depdning on the game mode
+        // so for now, we will create user_id var and assign it a temp value// we will also assume the player has chosen a game mode, and will run a different method depdning on the game mode
         // for now, we will create a random mode and call it
 //        random_play();
-        DB_SetUp.createNewDatabase();
-        DB_CreateTables.CreateStandardTables();
+//        DB_SetUp.createNewDatabase();
+//        DB_CreateTables.CreateStandardTables();
+        random_play();
     }
 
 
@@ -25,6 +23,7 @@ public class playLoop {
         }
     }
     public static void random_play(){
+        String user_ID = "sean";
         Scanner scan = new Scanner(System.in);
         // take all cards from database, create array of objects, shuffle array, and then play through the array, remoiving cards as they are played
 
@@ -53,9 +52,10 @@ public class playLoop {
             // check if correct index (i.e index +1)
             if (user_answer == cards_array[i].Question_correct_answer + 1){
                 // if correct, add 1 to score, and add win to scores_db and change score_db by 1
-                //                DB_ScoreInteract.addWin(user_id, cards_array[i].card_id);
-//                DB_ScoreInteract.changeScore(user_id, 1);
-                score++;
+                DB_ScoreInteract.addWin(user_ID, cards_array[i].card_id);
+                score++; // local score += 1
+
+
                 System.out.println("\nCorrect!");
                 try {
                     Thread.sleep(3000);
@@ -66,8 +66,7 @@ public class playLoop {
             }
             else {
                 // if incorrect, add loss to scores_db and change score_db by -1
-//                DB_ScoreInteract.addLoss(user_id, cards_array[i].card_id);
-//                DB_ScoreInteract.changeScore(user_id, -1);
+                DB_ScoreInteract.addLoss(user_ID, cards_array[i].card_id);
                 System.out.println("\nIncorrect!");
                 // wait 3 seconds
                 try {
@@ -83,5 +82,64 @@ public class playLoop {
 
 
     }
+    public static void increasingDifficulty_play(){
+        String user_ID = "sean";
+        Scanner scan = new Scanner(System.in);
+        // take all cards from database, create array of objects, shuffle array, and then play through the array, remoiving cards as they are played
 
+        card[] cards_array = DB_CardInteract.allCardsIncreasingDifficulty();
+        int score = 0;
+
+        // loop through the array,
+        for (int i = 0; i < cards_array.length; i++) {
+            // show card
+            // enter 1, 2 ,3 ,4
+            // check if correct index (i.e index +1)
+            // if correct, add 1 to score, and add win to scores_db and change score_db by 1
+            // if incorrect, add loss to scores_db and change score_db by -1
+
+            // show card
+
+            System.out.println(cards_array[i].Question_content);
+            for (int answer = 0; answer < cards_array[i].Question_answers_arr.length; answer++) {
+                System.out.println(answer + 1 + ": \t" + cards_array[i].Question_answers_arr[answer]);
+            }
+
+            // enter 1, 2 ,3 ,4
+            System.out.print("Enter your answer: ");
+            int user_answer = scan.nextInt();
+
+            // check if correct index (i.e index +1)
+            if (user_answer == cards_array[i].Question_correct_answer + 1){
+                // if correct, add 1 to score, and add win to scores_db and change score_db by 1
+                DB_ScoreInteract.addWin(user_ID, cards_array[i].card_id);
+                score++; // local score += 1
+
+
+                System.out.println("\nCorrect!");
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            else {
+                // if incorrect, add loss to scores_db and change score_db by -1
+                DB_ScoreInteract.addLoss(user_ID, cards_array[i].card_id);
+                System.out.println("\nIncorrect!");
+                // wait 3 seconds
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
+
+
+
+    }
 }
