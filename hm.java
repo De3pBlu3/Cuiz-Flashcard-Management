16b2e6b157e2331a4c5388f4d4a48ef99ce61553
unknown <23371323@studentmail.ul.ie>
2023-10-21 13:41:51 +0100

    db integration with gui

--- hm.java
+++ hm.java
@@ -4,7 +4,9 @@ import javafx.application.Application; //Setting up!! Make sure you dont have sr
 	import javafx.event.EventHandler;
 	import javafx.scene.Scene;
 	import javafx.scene.control.Button;
+	import javafx.stage.Popup;
 	import javafx.scene.layout.GridPane;
+	import javafx.scene.layout.StackPane;
 	import javafx.scene.layout.VBox;
 	import javafx.stage.Stage;
 	import javafx.scene.control.Label;
@@ -13,21 +15,25 @@ import javafx.application.Application; //Setting up!! Make sure you dont have sr
 	import javafx.scene.control.TextField;
 	import javafx.scene.control.PasswordField;
 
+import java.util.Objects;
+
+import javafx.animation.KeyFrame;
+	import javafx.animation.Timeline;
+	import javafx.scene.paint.Color;
+	import javafx.util.Duration;
 
 	public class hm extends Application {
-		Scene home, login, signUp; // All scenes
+		Scene home, login, signUp, Fail; // All scenes
 		
 		
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
 			        launch(args);  //method in application class that sets up javafx app (setup)
 			    }
-
 			    @Override
-			    public void start(Stage window) throws Exception {
+		public void start(Stage window) throws Exception {
 			        window.setTitle("The Quiz Logon"); //Window title
-			        //String user = "";
-			        //String pass = "";
+			       
 			    //Labels and buttons creation  
 	//------------------------------------------------------------------------------------//	        
 			        Label Intro = new Label("Welcome to the quiz! Please Login or Sign up");
@@ -49,20 +55,40 @@ import javafx.application.Application; //Setting up!! Make sure you dont have sr
 			        
 			        Button confirmButton = new Button("Confirm");
 			        confirmButton.setOnAction(e -> {
-			        	String user = usernameField.getText();
+			        	String user = usernameField.getText(); 		//Changes user input into String to check in db
 			        	String pass = passwordField.getText();
-			        		
-						while (DB_UserInteract.loginCheck(user, pass) == false) { //loop to allow user to attempt login again after inserting incorrect details
-							if (DB_UserInteract.loginCheck(user, pass)) { //Checks whether user name and password exists
-								new TextField("Successful login");
-							}else {
-								new TextField("Username or password does not match. Try again"); //loop continues
-							}
-						}
+		    		
+			        	if (DB_UserInteract.loginCheck(user, pass)== false) { 	//Checks whether user name and password exists
+			        		colorChange(usernameField, passwordField); 			//If it doesnt exist then go red
+			        		System.out.print("Username or password does not match. Try again"); //loop continues
+			        	}else {
+			        		System.out.print("Successful login");
+			        		//Add change scene to main quiz page
+			        	}
 			        });
 			        
 			        Button createAccountButton = new Button("Create Account");
-			        //createAccountButton.setOnAction(e -> pass = passwordField);
+			        createAccountButton.setOnAction(e -> {
+			        	String user = newUsernameField.getText(); 		//Changes user input into String to check in db
+			        	String pass = newPasswordField.getText();
+			        	String confirmPass = confirmPasswordField.getText();
+			        	
+			        	if (Objects.equals(confirmPass, pass)) { //tests whether the passwords  are equal
+							if (DB_UserInteract.insert(user, pass)){ // new user name and password stored if it doesn't exist
+							//temp = true; // ends loop
+							System.out.println("New user created successfully");}
+							//Add change scene to main quiz page
+							else {
+								System.out.println("Something has gone wrong, most likely the username already exists. Try again. "); //user exists and was not stored again
+								//PopUp for taken user
+							}
+						}else {
+							System.out.println("Passwords do not match.. ");
+							//method to make passwords go red
+							}
+			        	
+			        	//window.setScene(Fail);
+			        });
 			       
 			        Button loginButton = new Button("Login");
 			        loginButton.setOnAction(e -> window.setScene(login));
@@ -76,7 +102,7 @@ import javafx.application.Application; //Setting up!! Make sure you dont have sr
 			        Button ReturnsignUpButton = new Button("Back to Sign Up");
 			        ReturnsignUpButton.setOnAction(e -> window.setScene(signUp));
 			        //You cannot use the same button on different scenes//
-	//------------------------------------------------------------------------------------//	        
+	//---------------------------------------------------------------------------------------------------------------------//	        
 			        Insets Offset = new Insets(10,10,10,10);
 			        
 			     //home layout
@@ -126,7 +152,14 @@ import javafx.application.Application; //Setting up!! Make sure you dont have sr
 			        SignUpLay.getChildren().addAll(ReturnloginButton, createAccountButton, newUsernameField, newPasswordField, confirmPasswordField);
 			        
 			        signUp = new Scene(SignUpLay, 450, 250);
-	//------------------------------------------------------------------------------------//	        
+			        
+			     //Failure layout
+			        StackPane FailureLay = new StackPane();
+			        FailureLay.setAlignment(Pos.CENTER);
+			        FailureLay.getChildren().add(new TextField("Username or password does not match. Try again"));
+			        
+			        Fail = new Scene(FailureLay, 300, 100);
+	//-------------------------------------------------------------------------------------------------------------------------//	        
   
 			        
 			    //Show the GUI
@@ -134,21 +167,41 @@ import javafx.application.Application; //Setting up!! Make sure you dont have sr
 			        window.show();
 			       
 			    }
+		public static void colorChange(TextField usernameField, PasswordField passwordField) {
+			Timeline colorChangeTimeline = new Timeline( //To store keyframes
+		            new KeyFrame(Duration.seconds(0), e -> { // Change the text field's background color for 2 seconds
+		                usernameField.setStyle("-fx-background-color: red;");
+		                passwordField.setStyle("-fx-background-color: red;");
+		            }),
+		            new KeyFrame(Duration.seconds(1), e -> {// Revert the color after 3 seconds
+		                usernameField.setStyle("");
+		                passwordField.setStyle("");
+		            }));
+		            colorChangeTimeline.setCycleCount(1); //How many times it plays the sequence
+		            colorChangeTimeline.playFromStart(); //play
+					
+		}
+		public static void failPopUp() {	
+		}
+	}	
 	
-	}
-	/* 
-	- Stage = entire window
+	
+	
+	
+/*	 
+  	- Stage = entire window
 	- Scene = everything inside window
 	- Button = an object
 	- buttonName.setText("text on button"
-	.setOnAction(this) = envokes handle method
+	- .setOnAction(this) = envokes handle method
 	- StackPane = object, simple layout
 	- Scene = object, parameters, first one is layout name and next two is size
-	.setScene(sceneName) = primary stage command
-	.show() = primary stage command, displays scene
-	implements EventHandler<ActionEvent> = tells it what to do when an event happens
-	event.getSource() = checks what the source of the event was 
+	- .setScene(sceneName) = primary stage command
+	- .show() = primary stage command, displays scene
+	- implements EventHandler<ActionEvent> = tells it what to do when an event happens
+	- event.getSource() = checks what the source of the event was 
 	OR 
-	.setOnAction(e -> THING TO DO) = e replaces the handle method, lambda
-Label = text
-	*/
+	- .setOnAction(e -> THING TO DO) = e replaces the handle method, lambda
+	- Label = text
+	- .setStyle 
+*/
