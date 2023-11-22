c56959f9983b23afe733e94a9343564b2b536076
unknown <23371323@studentmail.ul.ie>
2023-11-22 15:54:51 +0000

    More DRY code

--- postgameScene.java
+++ postgameScene.java
@@ -2,6 +2,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Control;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.Background;
@@ -13,66 +14,45 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
+
+import java.lang.reflect.Method;
+
 public class postgameScene extends Scene {
 
         public postgameScene(Stage primaryStage, int score) {
+            //main template
             super(new VBox(), 440, 350);
-            
+
+            //cosmetic aspects
             Background transparentBackground = new Background(new BackgroundFill(Color.TRANSPARENT, CornerRadii.EMPTY, javafx.geometry.Insets.EMPTY));
             Background orangeBackground = new Background(new BackgroundFill(Color.rgb(232, 123, 56), CornerRadii.EMPTY, javafx.geometry.Insets.EMPTY));
             DropShadow dropShadow = new DropShadow();
        	 
-            //postGame contols
+            //postGame controls
             Label gameOverLabel = new Label("Game Over");
-            gameOverLabel.setFont(Font.font("ADLam Display", FontWeight.NORMAL, 24));
-            gameOverLabel.setTextFill(Color.WHITE);
-            gameOverLabel.setBackground(transparentBackground);
-            gameOverLabel.setEffect(dropShadow);
-            gameOverLabel.setTextAlignment(TextAlignment.CENTER);
-            gameOverLabel.setAlignment(Pos.CENTER);
+            applyCosmeticsLabel(gameOverLabel, transparentBackground, dropShadow);
 
             Label scoreLabel = new Label("Your total score is:");
-            scoreLabel.setFont(Font.font("ADLam Display", FontWeight.NORMAL, 20));
-            scoreLabel.setTextFill(Color.WHITE);
-            scoreLabel.setBackground(transparentBackground);
-            scoreLabel.setEffect(dropShadow);
-            scoreLabel.setTextAlignment(TextAlignment.CENTER);
-            scoreLabel.setAlignment(Pos.CENTER);
+            applyCosmeticsLabel(gameOverLabel, transparentBackground, dropShadow);
 
-            Label scoreDisplayLabel = new Label(""+ score + "");
-            scoreDisplayLabel.setFont(Font.font("ADLam Display", FontWeight.NORMAL, 15));
-            scoreDisplayLabel.setTextFill(Color.WHITE);
-            scoreDisplayLabel.setBackground(transparentBackground);
-            scoreDisplayLabel.setEffect(dropShadow);
-            scoreDisplayLabel.setTextAlignment(TextAlignment.CENTER);
-            scoreDisplayLabel.setAlignment(Pos.CENTER);
+            Label scoreDisplayLabel = new Label(score);
+            applyCosmeticsLabel(gameOverLabel, transparentBackground, dropShadow);
 
             Button viewStatsButton = new Button("View Statistics");
-            viewStatsButton.setOnAction(e -> {
-                    Stage stage = (Stage) viewStatsButton.getScene().getWindow();
-            primaryStage.setScene(statGUI.createScene(stage));
-            primaryStage.setFullScreen(true);
-        });
-            viewStatsButton.setFont(Font.font("ADLam Display", FontWeight.NORMAL, 15));
-            viewStatsButton.setTextFill(Color.WHITE);
-            viewStatsButton.setBackground(orangeBackground);
-            viewStatsButton.setEffect(dropShadow);
-            viewStatsButton.setTextAlignment(TextAlignment.CENTER);
-            viewStatsButton.setAlignment(Pos.CENTER);
+            applyCosmeticsButton(viewStatsButton, orangeBackground, dropShadow);
+                viewStatsButton.setOnAction(e -> {
+                    primaryStage.setScene(statGUI.createScene(primaryStage));
+                    primaryStage.setFullScreen(true);
+                });
+
             Button returnToMainButton = new Button("Return to Main Menu");
-            returnToMainButton.setOnAction(e -> {
-                Stage stage = (Stage) returnToMainButton.getScene().getWindow();
-            primaryStage.setScene(mainmenuScene.createScene(stage));
-            primaryStage.setFullScreen(true);
-        });
-            returnToMainButton.setFont(Font.font("ADLam Display", FontWeight.NORMAL, 15));
-            returnToMainButton.setTextFill(Color.WHITE);
-            returnToMainButton.setBackground(orangeBackground);
-            returnToMainButton.setEffect(dropShadow);
-            returnToMainButton.setTextAlignment(TextAlignment.CENTER);
-            returnToMainButton.setAlignment(Pos.CENTER);
-            Insets offset = new Insets(10,10,10,10);
-            
+            applyCosmeticsButton(returnToMainButton, orangeBackground, dropShadow);
+                returnToMainButton.setOnAction(e -> {
+                    primaryStage.setScene(mainmenuScene.createScene(primaryStage));
+                    primaryStage.setFullScreen(true);
+                });
+
+            //scene layout
             VBox PostGameLay = new VBox();
             PostGameLay.setStyle("-fx-background-color: #FFD966;");
             PostGameLay.setAlignment(Pos.CENTER);
@@ -87,4 +67,24 @@ public class postgameScene extends Scene {
         public static Scene createScene(Stage primarystage, int score) {
             return new postgameScene(primarystage, score);
         }
+
+        public static void applyCosmeticsLabel(Label label, Background transparentBackground, DropShadow dropShadow){
+            label.setFont(Font.font("ADLam Display", FontWeight.NORMAL, 24));
+            label.setTextFill(Color.WHITE);
+            label.setBackground(transparentBackground);
+            label.setEffect(dropShadow);
+            label.setTextAlignment(TextAlignment.CENTER);
+            label.setAlignment(Pos.CENTER);
+        }
+
+        public static void applyCosmeticsButton(Button button, Background orangeBackground, DropShadow dropShadow){
+            button.setFont(Font.font("ADLam Display", FontWeight.NORMAL, 15));
+            button.setTextFill(Color.WHITE);
+            button.setBackground(orangeBackground);
+            button.setEffect(dropShadow);
+            button.setTextAlignment(TextAlignment.CENTER);
+            button.setAlignment(Pos.CENTER);
+        }
+
+
 }
